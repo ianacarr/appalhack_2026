547c752903d8561da3f79f2f6332ce3a68a698e5
Meetric <143140156+ianacarr@users.noreply.github.com>
2026-03-21 19:35:47 -0400

    campfire visuals

--- src/main.js
+++ src/main.js
@@ -1,28 +1,108 @@
 import * as THREE from "three";
+import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js"
+
+
+// RENDERING CODE BELOW 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
+	30,
+	window.innerWidth / window.innerHeight,
+	0.0001,
+	10,
 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const loader = new GLTFLoader();
+const gltf = await loader.loadAsync("/models/stylized_campfire/scene.gltf");
+gltf.scene.traverse(function(child) {
+	if (child.isMesh) {
+		child.receiveShadow = true;
+		child.castShadow = true;
+	}
+});
+//
+let fireplace_pos = new THREE.Vector3(0.075, 0.005, 0.04);
 
-camera.position.z = 5;
+scene.add(gltf.scene);
+//renderer.setClearColor(0xffffff, 1);
+//camera.position = fireplace_pos + THREE.Vector3
+camera.position.set(0.2, 0.07, 0.07);
+
+camera.lookAt(fireplace_pos.x, fireplace_pos.y, fireplace_pos.z - 0.005);
+
+const light = new THREE.PointLight(0xF05000, 0.1);
+light.distance = 0.1;
+light.castShadow = true;
+light.position.set(fireplace_pos.x, fireplace_pos.y, fireplace_pos.z);
+scene.add(light);
+
+// fireplace particles
+let fire_material = new THREE.PointsMaterial({
+	size: 0.008,
+	sizeAttenuation: true,
+	color: new THREE.Color(1, 0.1, 0, 0),
+	transparent: true,
+	opacity: 0.5
+});
+
+class Particle {
+	constructor () {
+		this.pos = new THREE.Vector3(
+			Math.random() * 0.01 - 0.005, 
+			Math.random() * 0.0 - 0, 
+			Math.random() * 0.01 - 0.005
+		);
+		this.pos.add(fireplace_pos);
+		this.vel = new THREE.Vector3(
+			Math.random() / 4000 - 1 / 8000, 
+			Math.random() * 0.0001 + 0.0002, 
+			Math.random() / 4000 - 1 / 8000
+		);
+	}
+}
+
+let num_particles = 200;
+let particles = [];
+for (let i = 0; i < num_particles; i++) {
+	particles.push(new Particle());
+
+	// on startup
+	particles[i].pos.y += Math.random() * 0.05;
+}
 
 function animate(time) {
-  cube.rotation.x = time / 2000;
-  cube.rotation.y = time / 1000;
+	// SLOWWWW LMFAO!!!!!!!!!!!
+	let vertices = [];
+	for (let i = 0; i < num_particles; i++) {
+		let p = particles[i];
+		p.vel.x *= 0.99; // x/z dampening
+		p.vel.z *= 0.99; // x/z dampening
+		p.pos.add(p.vel);
+		if (p.pos.y > 0.05) {
+			p = new Particle();
+			particles[i] = p;
+		}
+		//console.log("penis");
+		//console.log(particles[i].pos);
+		vertices.push(p.pos.x);
+		vertices.push(p.pos.y);
+		vertices.push(p.pos.z);
+	}
+	
+	const geometry = new THREE.BufferGeometry();
+	geometry.setAttribute(
+		"position", 
+		new THREE.BufferAttribute(new Float32Array(vertices), 3)
+	);
 
-  renderer.render(scene, camera);
+	const mesh = new THREE.Points(geometry, fire_material);
+	scene.add(mesh);
+	renderer.render(scene, camera);
+	scene.remove(mesh);
 }
+
+renderer.setAnimationLoop(animate);
