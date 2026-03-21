db9f198283f4107a548f471aa924c0039d41bc17
nyukuru <nicholasod@proton.me>
2026-03-21 17:42:18 -0400

    a

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -11,7 +12,7 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
+document.querySelector("#appCanvas").appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
