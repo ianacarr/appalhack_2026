ebb078eb5bcbfe9881d27e9ea20d2ff17d8f5db3
nyukuru <nicholasod@proton.me>
2026-03-22 07:13:36 -0400

    a

--- src/scene/models.js
+++ src/scene/models.js
@@ -1,4 +1,3 @@
-import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const loader = new GLTFLoader();
@@ -12,14 +11,6 @@ export function loadModels(scene, fireplace_pos) {
         child.castShadow = true;
       }
     });
-    scene.add(model);
-  });
-
-  loader.load("/models/signpost/SignPostglb.glb", (gltf) => {
-    const model = gltf.scene;
-    model.position.set(0.075, 0.005, 0);
-    model.scale.set(0.01, 0.01, 0.01);
-    model.rotation.set(0, Math.PI / 2 - 0.45, 0);
     scene.add(model);
   });
 }
